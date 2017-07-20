a18869ec5d7e8a1e8b25dd3fdc7a99e892109a7c
R. Bassett Jr. (Tatwi) <rbassettjr@tpot.ca>
2017-07-19 22:56:24 -0400

    - Converted Merchant Yivvits weapons to crafted versions. - Quality is better than what is sold by BazaarBot, which is reflected in the higher prices. - Possible to further boost the quality with the proper relationships!

--- MMOCoreORB/bin/scripts/screenplays/hondo/merchants/weapons_yivvits.lua
+++ MMOCoreORB/bin/scripts/screenplays/hondo/merchants/weapons_yivvits.lua
@@ -11,15 +11,15 @@ YivvitsSP = ScreenPlay:new {
 		{name="thug", npcStanding=-3000, priceAdjust=30}  -- Enemy
 	},
 	goods = {
-		{optName="bounty_hunter", cost=5000, itemName="Bounty Hunter Rifle", items={"object/weapon/ranged/rifle/rifle_lightning.iff"}}, 
-		{optName="rifleman", cost=5000, itemName="Rifleman Laser Rifle", items={"object/weapon/ranged/rifle/rifle_laser_noob.iff"}},
-		{optName="tka", cost=5000, itemName="Teras Kasi Artist Vibroknuckler", items={"object/weapon/melee/special/vibroknuckler.iff"}}, 
-		{optName="carbineer", cost=5000, itemName="Carbineer Laser Carbine", items={"object/weapon/ranged/carbine/carbine_laser.iff"}},
-		{optName="commando", cost=5000, itemName="Commando Launcher Pistol", items={"object/weapon/ranged/pistol/pistol_launcher.iff"}},
-		{optName="fencer", cost=5000, itemName="Fencer Ryyk Blade", items={"object/weapon/melee/sword/sword_blade_ryyk.iff"}},
-		{optName="pikeman", cost=5000, itemName="Pikeman Polearm", items={"object/weapon/melee/polearm/polearm_vibro_axe.iff"}},
-		{optName="pistoleer", cost=5000, itemName="Pistoleer Republic Blaster", items={"object/weapon/ranged/pistol/pistol_republic_blaster.iff"}},
-		{optName="swordsman", cost=5000, itemName="Swordsman Vibroaxe", items={"object/weapon/melee/axe/axe_vibroaxe.iff"}}
+		{optName="bounty_hunter", cost=59950, itemName="Rifleman Lightning Rifle", crateSize=1, quality=65, altTemplate=0, items={"object/draft_schematic/weapon/rifle_lightning.iff"}}, 
+		{optName="rifleman", cost=29950, itemName="Rifleman Laser Rifle", crateSize=1, quality=80, altTemplate=0, items={"object/draft_schematic/weapon/rifle_blaster_laser_rifle.iff"}},
+		{optName="tka", cost=29950, itemName="Teras Kasi Artist Vibroknuckler", crateSize=1, quality=75, altTemplate=0, items={"object/draft_schematic/weapon/knuckler_vibro.iff"}}, 
+		{optName="carbineer", cost=29950, itemName="Carbineer Laser Carbine", crateSize=1, quality=67, altTemplate=0, items={"object/draft_schematic/weapon/rifle_light_blaster_laser_carbine.iff"}},
+		{optName="commando", cost=29950, itemName="Commando Launcher Pistol", crateSize=1, quality=67, altTemplate=0, items={"object/draft_schematic/weapon/pistol_launcher.iff"}},
+		{optName="fencer", cost=29950, itemName="Fencer Ryyk Blade", crateSize=1, quality=80, altTemplate=0, items={"object/draft_schematic/weapon/sword_ryyk_blade.iff"}},
+		{optName="pikeman", cost=29950, itemName="Pikeman Polearm", crateSize=1, quality=65, altTemplate=0, items={"object/draft_schematic/weapon/poleaxe_vibro.iff"}},
+		{optName="pistoleer", cost=29950, itemName="Pistoleer Republic Blaster", crateSize=1, quality=70, altTemplate=0, items={"object/draft_schematic/weapon/pistol_republic_blaster.iff"}},
+		{optName="swordsman", cost=29950, itemName="Swordsman Vibroaxe", crateSize=1, quality=68, altTemplate=0, items={"object/draft_schematic/weapon/axe_vibro.iff"}}
 	}
 }
 
